178130e2b152de1e50987de5fcf026d780f2c98c
diego <diegogelvis14@gmail.com>
2024-12-24 21:13:06 -0400

    deploy

--- mi_portafolio/settings.py
+++ mi_portafolio/settings.py
@@ -140,6 +140,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://mrlechoza.github.io/Porfolio-frontend/",
 ]
 
-ALLOWED_HOSTS = ['https://porfolio-backend-ujlh.onrender.com']
+ALLOWED_HOSTS = ['https://porfolio-backend-1.onrender.com']
+
 
 CORS_ALLOW_ALL_ORIGINS = True
